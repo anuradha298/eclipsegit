83c94198ec735cf929ca9e0a6a0c7d77310fc641
Anuradha298 <Anuradhavmane@gmail.com>
2019-03-27 12:22:17 +0530

    First change

--- gIT/src/PACKAGE/WELCOME.java
+++ gIT/src/PACKAGE/WELCOME.java
@@ -5,6 +5,7 @@ public class WELCOME {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("wELCOME ON BOARD");
+System.out.println("code is pushed on git");
 	}
 
 }
