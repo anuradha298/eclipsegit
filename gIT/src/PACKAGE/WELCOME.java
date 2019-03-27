51b1b0c3f0853efaedf70003a8aa25fbc42220c3
Anuradha298 <Anuradhavmane@gmail.com>
2019-03-27 13:40:36 +0530

    2nd commit

--- gIT/src/PACKAGE/WELCOME.java
+++ gIT/src/PACKAGE/WELCOME.java
@@ -6,6 +6,7 @@ public class WELCOME {
 		// TODO Auto-generated method stub
 System.out.println("wELCOME ON BOARD");
 System.out.println("code is pushed on git");
+System.out.println("Test");
 	}
 
 }
